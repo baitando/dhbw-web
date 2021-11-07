1cdee51ff9062a2c42aaefd8bc251453c3a4ee21
Andreas Hirsch <andreas.hirsch@mimacom.com>
2021-11-07 21:49:06 +0100

    Demo 02a_testing-selenium

---
+++ 02a_testing-selenium/test/create.js
@@ -0,0 +1,33 @@
+const {Builder, By} = require("selenium-webdriver");
+const assert = require("assert");
+const fs = require("fs");
+
+describe('Create Page', function () {
+    this.timeout(6000);
+    let driver
+
+    beforeEach(async function () {
+        driver = await new Builder().forBrowser('chrome').build()
+        await driver.get(`file://${process.cwd()}/../01e_javascript/result/anlage.html`);
+    })
+
+    afterEach(async function () {
+        await driver.quit()
+    })
+
+    it('create task', async function () {
+        await driver.findElement(By.id("title")).sendKeys("Meine erste Aufgabe");
+        await driver.findElement(By.id("notes")).sendKeys("Auf gar keinen Fall vergessen!");
+        await driver.findElement(By.id("due")).sendKeys("26.10.2021");
+        await driver.findElement(By.id("responsible")).sendKeys("Max Mustermann");
+
+        let image = await driver.takeScreenshot();
+        await fs.writeFileSync('screenshot.png', image, 'base64');
+
+        await driver.findElement(By.linkText("Anlegen")).click();
+
+        let items = await driver.findElements(By.css("#tasks li"));
+
+        assert.equal(items.length, 1);
+    })
+})
---
+++ 02a_testing-selenium/test/startepage.js
@@ -0,0 +1,35 @@
+const {Builder, By} = require("selenium-webdriver");
+const assert = require("assert");
+
+describe('Startpage', function () {
+    this.timeout(6000);
+    let driver
+
+    beforeEach(async function () {
+        driver = await new Builder().forBrowser('chrome').build();
+        await driver.get(`file://${process.cwd()}/../01e_javascript/result/index.html`);
+    })
+    afterEach(async function () {
+        await driver.quit();
+    })
+
+    it('navigate to app using teaser button', async function () {
+
+        await driver.findElement(By.linkText("Direkt loslegen")).click();
+        let headline = await driver.findElement(By.tagName("h1")).getText();
+
+        assert.equal(headline, "Offene Aufgaben");
+    })
+
+    it('navigate to app using main navigation', async function () {
+        await driver.findElement(By.linkText("App")).click();
+        let headline = await driver.findElement(By.tagName("h1")).getText();
+        assert.equal(headline, "Offene Aufgaben");
+    })
+
+    it('navigate to prices using main navigation', async function () {
+        await driver.findElement(By.linkText("Preise")).click();
+        let headline = await driver.findElement(By.className("teaser-title")).getText();
+        assert.equal(headline, "Ein Angebot, das mit den Anforderungen wächst");
+    })
+})
