af3720184ba78f0c7a74df467cc106ec5adcfa0b
Andreas Hirsch <andreas.hirsch@baitando.com>
2022-11-28 06:04:21 +0100

    Extend Mocha and Chai sample

--- 02a_testing-mocha-chai/sample/result/greeter_test.js
+++ 02a_testing-mocha-chai/sample/result/greeter_test.js
@@ -1,9 +1,19 @@
 describe("greet", function () {
 
     const expect = chai.expect;
+    const assert = chai.assert;
+    chai.should();
 
-    it('should retrun correct greeting', function () {
+    it('should return correct greeting (expect style)', function () {
         expect(getGreeting("Miriam")).to.equal("Hello Miriam");
+    });
+
+    it('should return correct greeting (assert style)', function () {
+        assert.equal(getGreeting("Miriam"), "Hello Miriam");
+    });
+
+    it('should return correct greeting (should style)', function () {
+        getGreeting("Miriam").should.equal("Hello Miriam");
     });
 
 });
